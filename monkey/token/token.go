cc645d46c54737d5f1ef92923a1cd9e9e6598275
HagaSpa <justicesparrow13@gmail.com>
2020-03-16 23:59:44 +0900

    imp lexer_test

---
+++ monkey/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL" // トークンが未知のもの
+	EOF     = "EOF"     // 終端文字
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1234
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
