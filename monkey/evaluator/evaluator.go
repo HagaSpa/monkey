61bcfa9b3c71a26949867176da6d0ff79febdb75
HagaSpa <justicesparrow13@gmail.com>
2020-04-15 17:20:16 +0900

    Add String join operator

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -174,6 +174,8 @@ func evalInfixExpression(
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s",
 			left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
@@ -255,6 +257,22 @@ func evalExpressions(
 		result = append(result, evaluated)
 	}
 	return result
+}
+
+// 中置式のうち、左項と右項がSTRING_OBJの場合
+// （文字列結合を想定）
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func isTruthy(obj object.Object) bool {
